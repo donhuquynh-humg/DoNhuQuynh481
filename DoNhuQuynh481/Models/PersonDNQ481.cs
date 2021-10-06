b25bd33336bea069f7b3e218201bdc90f3028f28
QUYNH DO NHU <1821050481@student.humg.edu.vn>
2021-10-06 08:03:05 +0700

    Create_Table_Person_DNQ481

---
+++ DoNhuQuynh481/Models/PersonDNQ481.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoNhuQuynh481.Models
+{
+    public class PersonDNQ481
+    {
+        [Key]
+        [Column(TypeName= "varchar")]
+        [StringLength(20)]
+        public int PersonId { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [StringLength(50)]
+        public int PersonName { get; set; }
+    }
+}
